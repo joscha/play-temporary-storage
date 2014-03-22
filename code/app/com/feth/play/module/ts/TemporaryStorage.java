9d150c4b201042e94280ac38a6245e550fdad9ca
Joscha Feth <joscha@feth.com>
2014-03-22 17:12:02 +0100

    Add some signature documentation

--- code/app/com/feth/play/module/ts/TemporaryStorage.java
+++ code/app/com/feth/play/module/ts/TemporaryStorage.java
@@ -5,15 +5,35 @@ import java.io.Serializable;
 
 public interface TemporaryStorage {
 
-	
-	public <T extends Serializable> T get(final String key);
-	public <T extends Serializable> void set(final String key, final T something);
 	/**
+	 * Gets an object from the temporary storage
 	 * 
-	 * @param key
-	 * @param something
-	 * @param expiration in seconds
+	 * @param key the unique key to find the object under
+	 * @return the object or null if the object was not found or the expiration date exceeded
+	 */
+	public <T extends Serializable> T get(final String key);
+	
+	/**
+	 * Puts an object into the temporary storage
+	 * 
+	 * @param key the unique key to register the object under. If this key has been used, the old value gets overwritten.
+	 * @param something The object to store
+	 */
+	public <T extends Serializable> void set(final String key, final T something);
+	
+	/**
+	 * Puts an object into the temporary storage with a defined expiration
+	 * 
+	 * @param key @see set()
+	 * @param something @see set()
+	 * @param expiration the expiration time of the object in the temporary storage in seconds
 	 */
 	public <T extends Serializable> void set(final String key, final T something, final int expiration);
+	
+	/**
+	 * Removes an object from the temporary storage
+	 * 
+	 * @param key the unique key of the object to be removed
+	 */
 	public void remove(final String key);
 }
