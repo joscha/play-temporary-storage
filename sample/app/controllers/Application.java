393a91535263ff994b6545d51a7b7aeaee8e5111
Joscha Feth <joscha@feth.com>
2014-03-22 17:00:13 +0100

    factor keys out

--- sample/app/controllers/Application.java
+++ sample/app/controllers/Application.java
@@ -11,24 +11,28 @@ import com.feth.play.module.ts.TemporaryStorage;
 
 public class Application extends Controller {
 
+	private static final String KEY_A = "a";
+	private static final String KEY_B = "b";
+	private static final String KEY_C = "c";
+
 	public static Result index() {
 		final TemporaryStorage cachedStorage = new CacheTemporaryStorage();
-		Integer a = cachedStorage.get("a");
-		cachedStorage.set("a", a == null ? a = 0 : ++a);
-		final String aRaw = "" + Cache.get("a");
+		Integer a = cachedStorage.get(KEY_A);
+		cachedStorage.set(KEY_A, a == null ? a = 0 : ++a);
+		final String aRaw = "" + Cache.get(KEY_A);
 
 		final TemporaryStorage sessionStorage = new SessionTemporaryStorage(
 				session());
-		Integer b = sessionStorage.get("b");
-		sessionStorage.set("b", b == null ? b = 0 : ++b);
-		final String bRaw = session("b");
+		Integer b = sessionStorage.get(KEY_B);
+		sessionStorage.set(KEY_B, b == null ? b = 0 : ++b);
+		final String bRaw = session(KEY_B);
 		
 
 		final TemporaryStorage encryptedSessionStorage = new SessionTemporaryStorage(
 				session(), true);
-		Integer c = encryptedSessionStorage.get("c");
-		encryptedSessionStorage.set("c", c == null ? c = 0 : ++c);
-		final String cRaw = session("c");		
+		Integer c = encryptedSessionStorage.get(KEY_C);
+		encryptedSessionStorage.set(KEY_C, c == null ? c = 0 : ++c);
+		final String cRaw = session(KEY_C);
 
 		return ok(index.render(a, aRaw, b, bRaw, c, cRaw));
 	}
